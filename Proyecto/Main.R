96f641316072ea0689705363adaa6131ff866747
J. Emmanuel Gil C <emmanuel_12_gc@hotmail.com>
2021-02-04 18:56:35 -0600

    Proyecto - Importacion de datos

--- Proyecto/Main.R
+++ Proyecto/Main.R
@@ -1,1 +1,11 @@
-asdf
+# Importar los archivos CSV
+customers <- read.csv("olist_customers_dataset.csv")
+geolocation <- read.csv("olist_geolocation_dataset.csv")
+order.items <- read.csv("olist_order_items_dataset.csv")
+order.payments <- read.csv("olist_order_payments_dataset.csv")
+order.reviews <- read.csv("olist_order_reviews_dataset.csv")
+orders <- read.csv("olist_orders_dataset.csv")
+products <- read.csv("olist_products_dataset.csv")
+sellers <- read.csv("olist_sellers_dataset.csv")
+category_names_translation <- read.csv("product_category_name_translation.csv")
+
